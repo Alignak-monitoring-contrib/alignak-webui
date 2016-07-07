f803815dcb243f186219d89807722a91b22b8b4f
Frédéric MOHIER <frederic.mohier@gmail.com>
2016-07-07 11:10:07 +0200

    Run script

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-uwsgi --wsgi-file alignak_webui.py --callable app --socket 0.0.0.0:5000 --protocol=http --enable-threads
+uwsgi --plugin python --wsgi-file alignak_webui.py --callable app --socket 0.0.0.0:8868 --protocol=http --enable-threads
