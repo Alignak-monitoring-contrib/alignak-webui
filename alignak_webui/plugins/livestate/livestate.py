b7081ae4421925146c8a6a5e858c2c3b3f4fec14
Frédéric MOHIER <frederic.mohier@gmail.com>
2017-03-26 11:16:17 +0200

    Add a livestate widget

--- alignak_webui/plugins/livestate/livestate.py
+++ alignak_webui/plugins/livestate/livestate.py
@@ -26,7 +26,7 @@
 import json
 from logging import getLogger
 
-from bottle import request, response
+from bottle import request, response, template
 
 from alignak_webui.utils.plugin import Plugin
 from alignak_webui.utils.helper import Helper
@@ -53,6 +53,27 @@ class PluginLivestate(Plugin):
                 'name': 'Livestate',
                 'route': '/livestate',
                 'view': 'livestate'
+            },
+            'get_livestate_widget': {
+                'name': 'Livestate widget',
+                'route': '/livestate/widget',
+                'method': 'POST',
+                'view': 'livestate_widget',
+                'widgets': [
+                    {
+                        'id': 'livestate',
+                        'for': ['external', 'dashboard'],
+                        'name': _('Livestate widget'),
+                        'template': 'livestate_widget',
+                        'icon': 'heartbeat',
+                        'description': _(
+                            '<h4>Livestate widget</h4>Displays the live state of '
+                            'the monitored system.'
+                        ),
+                        'picture': 'static/img/livestate_widget.png',
+                        'options': {}
+                    }
+                ]
             }
         }
 
@@ -87,3 +108,110 @@ class PluginLivestate(Plugin):
             'layout': request.app.config.get('livestate_layout', 'table'),
             'title': request.query.get('title', _('Livestate'))
         }
+
+    def get_livestate_widget(self, embedded=False, identifier=None, credentials=None):
+        # pylint: disable=too-many-locals
+        """Get the livestate widget"""
+        user = request.environ['beaker.session']['current_user']
+        webui = request.app.config['webui']
+        datamgr = webui.datamgr
+
+        panels = datamgr.get_user_preferences(user, 'livestate', {})
+
+        ls = Helper.get_html_livestate(datamgr, panels, int(request.query.get('bi', -1)),
+                                       request.query.get('search', {}), actions=user.is_power())
+
+        # Widget options
+        widget_id = request.params.get('widget_id', '')
+        if widget_id == '':
+            return self.webui.response_invalid_parameters(_('Missing widget identifier'))
+
+        widget_place = request.params.get('widget_place', 'dashboard')
+        widget_template = request.params.get('widget_template', 'elements_table_widget')
+        widget_icon = request.params.get('widget_icon', 'plug')
+        logger.debug("Searching a widget %s for: %s (%s)",
+                     widget_id, widget_place, widget_template)
+
+        # Search in the application widgets (all plugins widgets)
+        options = {}
+        for widget in self.webui.get_widgets_for(widget_place):
+            logger.debug("Found widget: %s (%s)", widget['name'], widget['id'])
+            if widget_id.startswith(widget['id']):
+                options = widget['options']
+                widget_template = widget['template']
+                widget_icon = widget['icon']
+                logger.debug("Widget %s found, template: %s, options: %s",
+                             widget_id, widget_template, options)
+                break
+        else:
+            logger.warning("Widget identifier not found: %s", widget_id)
+            return self.webui.response_invalid_parameters(_('Unknown widget identifier'))
+
+        # Search in the saved dashboard widgets
+        saved_widget = None
+        saved_widgets = datamgr.get_user_preferences(user, 'dashboard_widgets', [])
+        for widget in saved_widgets:
+            if widget_id == widget['id']:
+                saved_widget = widget
+                logger.info("Saved widget found: %s", saved_widget)
+                break
+
+        # Widget freshly created
+        tmp_options = []
+        if not saved_widget or 'options' not in saved_widget:
+            for option in options:
+                tmp_options.append("%s=%s" % (option, options[option]['value']))
+            saved_options = '|'.join(tmp_options)
+        else:
+            saved_options = saved_widget['options']
+
+        tmp_options = []
+        logger.info("Saved widget options: %s", saved_options)
+        for option in saved_options.split('|'):
+            option = option.split('=')
+            logger.info("- saved option: %s", option)
+            if len(option) > 1:
+                if request.params.get(option[0], option[1]) != option[1]:
+                    tmp_options.append("%s=%s" % (option[0], request.params.get(option[0])))
+                    options[option[0]]['value'] = request.params.get(option[0])
+                else:
+                    tmp_options.append("%s=%s" % (option[0], option[1]))
+                    options[option[0]]['value'] = option[1]
+
+        new_options = '|'.join(tmp_options)
+
+        if saved_options != new_options:
+            logger.info("Widget %s new options: %s", widget_id, new_options)
+
+            # Search for the dashboard widgets
+            saved_widgets = datamgr.get_user_preferences(user, 'dashboard_widgets', [])
+            for widget in saved_widgets:
+                if widget_id.startswith(widget['id']):
+                    widget['options'] = new_options
+                    datamgr.set_user_preferences(user, 'dashboard_widgets', saved_widgets)
+                    logger.info("Widget new options saved!")
+                    break
+        saved_options = new_options
+
+        title = request.params.get('title', _('Elements'))
+
+        # Use required template to render the widget
+        logger.info("Rendering widget %s", widget_id)
+        return template('_widget', {
+            'widget_id': widget_id,
+            'widget_name': widget_template,
+            'widget_place': widget_place,
+            'widget_template': widget_template,
+            'widget_icon': widget_icon,
+            'widget_uri': request.urlparts.path,
+
+            'plugin_parameters': self.plugin_parameters,
+
+            'livestate': ls,
+
+            'options': options,
+            'title': title,
+            'embedded': embedded,
+            'identifier': identifier,
+            'credentials': credentials
+        })
